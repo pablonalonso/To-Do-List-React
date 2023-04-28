721c824af0260b1c94c43d7f0bbc061fb2f139a7
Alonso, Pablo <pabloalonnso21@gmail.com>
2023-04-28 12:12:39 -0300

    Updates

--- src/data/list-of-tasks.jsx
+++
@@ -1,8 +0,0 @@
-export const listOfTasks = [
-    {id: 0, activity: 'Hacer la cama', time: '08:00', done: false},
-    {id: 1, activity: 'Pasear al perro', time: '10:00', done: false},
-    {id: 2, activity: 'Estudiar ingles', time: '11:00', done: false},
-    {id: 3, activity: 'Leer un libro', time: '12:00', done: true},
-    {id: 4, activity: 'Ir de compras', time: '14:00', done: true},
-    {id: 5, activity: 'Hacer ejercicio', time: '16:00', done: true},
-]
